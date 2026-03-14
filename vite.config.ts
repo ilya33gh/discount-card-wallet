873330f6ac006a4fd752cbeb62be55aac3c1d716
ilya33gh <ilya33gh@users.noreply.github.com>
2026-03-14 22:19:26 +0300

    Add legacy build for mobile compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+      modernPolyfills: true
+    })
+  ],
+  build: {
+    target: "es2017"
+  },
   server: {
     host: true,
     port: 5173
